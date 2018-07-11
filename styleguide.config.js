48600499015ddb337cbd75bfd05798d49e6159ce
Maxim Sukharev <maxim@sourced.tech>
2018-07-11 15:15:02 +0200

    change style loader

--- styleguide.config.js
+++ styleguide.config.js
@@ -11,7 +11,7 @@ module.exports = {
 				},
 				{
 					test: /\.css$/,
-					loader: 'style-loader!css-loader?modules&importLoaders=1',
+					loader: 'style-loader!css-loader?importLoaders=1',
 				},
 			],
 		},
